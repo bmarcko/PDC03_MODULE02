b94d2d18addef553b3954b037d3fbbbc8885dda7
bmarcko <bobbymarckocruz@gmail.com>
2023-03-03 10:27:55 +0800

    updated activity5

--- PDC03_MODULE02/PDC03_MODULE02/activity5.xaml.cs
+++ PDC03_MODULE02/PDC03_MODULE02/activity5.xaml.cs
@@ -28,6 +28,7 @@ namespace PDC03_MODULE02
                 employees.Add(new employee { DisplayName = "Jell-o Mangune", Position = "Vice President", ProfileImage = "image2.jpeg" });
                 employees.Add(new employee { DisplayName = "Calvin Klein Pangilinan", Position = "Secretary", ProfileImage = "image3.jpg" });
                 employees.Add(new employee { DisplayName = "Ross Weasley", Position = "Treasurer", ProfileImage = "https://th.bing.com/th/id/OIP.hGIKluXLZvu4xwPMgCdImAHaJr?w=185&h=242&c=7&r=0&o=5&pid=1.7" });
+                employees.Add(new employee { DisplayName = "Aaron Echon", Position = "Auditor" });
             }
         }
     }
